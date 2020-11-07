d0f6817638b691d3c2b0d2f0c052571b105cc93e
OpatileKelobang <kelobang.opatile@gmail.com>
2020-11-07 13:24:05 +0200

    Added @Configuration annotation

--- src/main/java/za/co/bogote/ecommerceproject/config/MyDataRestConfig.java
+++ src/main/java/za/co/bogote/ecommerceproject/config/MyDataRestConfig.java
@@ -1,11 +1,13 @@
 package za.co.bogote.ecommerceproject.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import za.co.bogote.ecommerceproject.entity.Product;
 import za.co.bogote.ecommerceproject.entity.ProductCategory;
 
+@Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
     @Override
